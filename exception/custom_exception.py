7d0672ac9ec02e242a0b0327e5bd4f8496640bee
Ajinkya Raskar <ajinkyaraskar@gmail.com>
2025-11-18 00:07:22 +0530

    changes in log file and files structure updated

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -1,31 +1,31 @@
-import sys
 import traceback
+import sys
 from logger.custom_logger import CustomLogger
-logger = CustomLogger().get_logger(__file__)
-
+logger=CustomLogger().get_logger(__file__)
 
 class DocumentPortalException(Exception):
-    """Custom Exception for Document Portal"""
-    def __init__(self,error_message:Exception,error_details:sys):
-        print(error_details.exc_info())
-        _,_,exc_tb=error_details.exc_info()
-        self.line_number=exc_tb.tb_lineno
+    """Custom exception for Document Portal"""
+    def __init__(self,error_message):
+        _,_,exc_tb= sys.exc_info()
         self.file_name=exc_tb.tb_frame.f_code.co_filename
-        self.error_message= str(error_message)
-        self.traceback_str = ''.join(traceback.format_exception(*error_details.exc_info()))
-
+        self.lineno=exc_tb.tb_lineno
+        self.error_message=str(error_message)
+        self.traceback_str = ''.join(traceback.format_exception(*error_details.exc_info())) 
+        
     def __str__(self):
-        return f"""
-        Error occurred in script: [{self.file_name}] at line number: [{self.line_number}]
+       return f"""
+        Error in [{self.file_name}] at line [{self.lineno}]
         Message: {self.error_message}
-        Traceback: 
+        Traceback:
         {self.traceback_str}
         """
-
-if __name__=="__main__":
+    
+if __name__ == "__main__":
     try:
-        a=1/0
+        # Simulate an error
+        a = 1 / 0
+        print(a)
     except Exception as e:
-        app_exc = DocumentPortalException(e,sys)
+        app_exc=DocumentPortalException(e)
         logger.error(app_exc)
-        raise app_exc from e
+        raise app_exc
